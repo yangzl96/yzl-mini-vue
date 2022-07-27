99b8313e65c5228df92b748bd58a16bd7c5c1a6c
yangzongliu <1065051770@qq.com>
2022-07-27 15:15:18 +0800

    修复diff bug

--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -233,7 +233,7 @@ export function createRenderer(options) {
           newIndex = keyToNewIndexMap.get(prevChild.key)
         } else {
           // 没有Key
-          for (let j = s2; j < e2; j++) {
+          for (let j = s2; j <= e2; j++) {
             if (isSameVNodeType(prevChild, c2[j])) {
               newIndex = j
               break
